1de2693b7ac9ae6015bc8a32ded463121ce8d66d
Alexandra Szvatko <alexandraszvatko@gmail.com>
2023-09-15 20:45:14 +0200

    basic layout

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+console.log("Hello Word");
+console.log("I am Szandi");
+//setInterval(()=>console.log("Hello"),1000);
+
+let x = 10; 
+let y = 5;
+//console.log(x+y);
+x + y;
+let z = "csésze"
+console.log(z);
+z = "Bögre"
+console.log(z);
